24319eb98f7532b5fa27f9e81c2ba3326c0a85a4
Navi <singhnav0309@gmail.com>
2026-01-25 22:32:31 +0530

    Add files via upload

---
+++ Mathematics_operations.py
@@ -0,0 +1,29 @@
+""" Perform Basic Mathematical Operations """
+
+num1 = input("Enter a first number: ")
+num2 = input("Enter a second number: ")
+print(num1)
+print(num2)
+
+# Addition
+Result = int(num1) + int(num2)
+print(Result)
+
+# Subtraction
+Result = int(num1) - int(num2)
+print(Result)
+
+# Multiplication
+Result = int(num1) * int(num2)
+print(Result)
+
+# Division
+Result = int(num1) / int(num2)
+print(Result)
+
+
+
+
+
+
+
---
+++ Personalised_Greeting.py
@@ -0,0 +1,17 @@
+"""
+Python Program for Greeting"""
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+FullName = first + " " + last
+print(FullName)
+print("Hello, ""FullName! " "Welcome " "to " "the Python")
+
+
+
+
+
+
+
+
+
+
